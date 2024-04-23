8beadbf9ffc4fddae20b69b963a94aba2255c03d
sophiakubisiak <167253903+sophiakubisiak@users.noreply.github.com>
2024-04-23 15:12:59 -0700

    Add files via upload

---
+++ R/MaxTemp.R
@@ -0,0 +1,10 @@
+#' Max Temps in Flagstaff, AZ
+#'
+#' A time series of daily maximum temperatures in Flagstaff, AZ.
+#' @format a data frame with 10882 observations
+#' \describe{
+#'   \item{Date}{The date of observation as a POSIX date format.}
+#'   \item{MaxTemp}{Daily maximum temperature in degrees Farhenheit.}
+#' }
+#' @source \url{www.ncdc.noaa.gov}
+"MaxTemp"
